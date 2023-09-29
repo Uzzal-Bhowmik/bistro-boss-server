204bf5c0d5f7d64bf5a0afa3545ee2565980468f
Uzzal Bhowmik <ubcreativity111@gmail.com>
2023-09-29 12:54:03 +0600

    user collection added and user related api added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 // mongo db
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5732rtt.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,10 +25,82 @@ async function run() {
   try {
     await client.connect();
 
+    const userCollection = client.db("bistroRestaurantDB").collection("users");
     const menuCollection = client.db("bistroRestaurantDB").collection("menu");
+    const reviewCollection = client
+      .db("bistroRestaurantDB")
+      .collection("reviews");
+    const cartCollection = client.db("bistroRestaurantDB").collection("cart");
 
+    // menu related api
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+
+    // review related api
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
+
+    // cart related api
+    app.get("/cart", async (req, res) => {
+      const email = req.query.email;
+
+      if (!email) {
+        res.send([{ error: "email not found" }]);
+      }
+
+      const result = await cartCollection.find({ email: email }).toArray();
+      res.send(result);
+    });
+
+    app.post("/cart", async (req, res) => {
+      const item = req.body;
+      const result = await cartCollection.insertOne(item);
+      res.send(result);
+    });
+
+    app.delete("/cart/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await cartCollection.deleteOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
+    // user related api
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find({}).toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+
+      // query to check if user already exists
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
+      }
+
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+
+      const updatedUser = {
+        $set: {
+          role: "admin",
+        },
+      };
+
+      const result = await userCollection.updateOne(filter, updatedUser);
       res.send(result);
     });
 
