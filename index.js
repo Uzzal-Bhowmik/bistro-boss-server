f1669b5fcedf501cf683bae67161c897161e4a04
Uzzal Bhowmik <ubcreativity111@gmail.com>
2023-10-02 01:08:32 +0600

    admin routes added and security implemented

--- index.js
+++ index.js
@@ -3,13 +3,38 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+const jwt = require("jsonwebtoken");
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
+// jwt token verification middleware
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.JWT_SECRET_TOKEN, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+
+    req.decoded = decoded;
+    next();
+  });
+};
+
 // mongo db
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { JsonWebTokenError } = require("jsonwebtoken");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5732rtt.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,6 +57,29 @@ async function run() {
       .collection("reviews");
     const cartCollection = client.db("bistroRestaurantDB").collection("cart");
 
+    // verifyAdmin middleware
+    const verifyAdmin = async (req, res, next) => {
+      const userEmail = req.decoded?.email;
+      const user = await userCollection.findOne({ email: userEmail });
+
+      if (user?.role !== "admin") {
+        return res
+          .status(401)
+          .send({ error: true, message: "forbidden access" });
+      }
+
+      next();
+    };
+
+    // jwt api
+    app.post("/jwt", (req, res) => {
+      const userEmail = req.body;
+      const token = jwt.sign(userEmail, process.env.JWT_SECRET_TOKEN, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
     // menu related api
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
@@ -45,11 +93,17 @@ async function run() {
     });
 
     // cart related api
-    app.get("/cart", async (req, res) => {
+    app.get("/cart", verifyJWT, async (req, res) => {
       const email = req.query.email;
 
-      if (!email) {
-        res.send([{ error: "email not found" }]);
+      if (!email || email === undefined) {
+        return res.send([]);
+      }
+
+      if (req.decoded?.email !== email) {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden access" });
       }
 
       const result = await cartCollection.find({ email: email }).toArray();
@@ -69,7 +123,11 @@ async function run() {
     });
 
     // user related api
-    app.get("/users", async (req, res) => {
+    // security layers for admin apis
+    // 0. hide admin routes only to admin and user routes to users using isAdmin ?
+    // 1. verifyJWT : so that api link is not open to everyone
+    // 2. use verifyAdmin(after verifyJWT) in all admin routes: to chk if the user is genuinely an Admin or not, to prevent going to the admin routes using browser url bar
+    app.get("/users", verifyJWT, verifyAdmin, async (req, res) => {
       const result = await userCollection.find({}).toArray();
       res.send(result);
     });
@@ -87,6 +145,19 @@ async function run() {
 
       const result = await userCollection.insertOne(user);
       res.send(result);
+    });
+
+    // api to check if user is an Admin
+    app.get("/users/admin/:email", verifyJWT, async (req, res) => {
+      const userEmail = req.params.email;
+      const user = await userCollection.findOne({ email: userEmail });
+      console.log(req.decoded);
+
+      if (req.decoded.email !== userEmail) {
+        return res.send({ isAdmin: false });
+      }
+
+      res.send({ isAdmin: user?.role === "admin" });
     });
 
     app.patch("/users/admin/:id", async (req, res) => {
